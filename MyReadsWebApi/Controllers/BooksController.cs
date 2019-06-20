2ef0921383946180ff81b0ee4ccdca097d411b47
Christopher Tso <christso@gmail.com>
2019-06-20 11:58:58 +1000

    initial commit

---
+++ MyReadsWebApi/Controllers/BooksController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.InMemory;
+using MyReadsWebApi.Models;
+
+namespace MyReadsWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly DataContext _context;
+    
+        public BooksController(DataContext context)
+        {
+            _context = context;
+
+            if (_context.Books.Count() == 0)
+            {
+                var book1 = new Book()
+                {
+                    Title = "The Linux Command Line",
+                    Shelf = "read",
+                    Id = "nggnmAEACAAJ",
+                    Authors = new List<Author>()
+                    {
+                        new Author() { Name = "William E. Shotts, Jr." }
+                    }
+                };
+                _context.Books.Add(book1);
+
+                var book2 = new Book()
+                {
+                    Title = "Learning Web Development with React and Bootstrap",
+                    Shelf = "currentlyReading",
+                    Id = "sJf1vQAACAAJ",
+                    Authors = new List<Author>
+                    {
+                        new Author() { Name = "Harmeet Singh" },
+                        new Author() { Name = "Mehul Bhatt" }
+                    }
+                };
+                _context.Books.Add(book2);
+
+                var book3 = new Book()
+                {
+                    Title = "The Cuckoo's Calling",
+                    Shelf = "wantToRead",
+                    Id = "evuwdDLfAyYC",
+                    Authors = new List<Author>
+                    {
+                        new Author() { Name = "Robert Galbraith" }
+                    }
+                };
+                _context.Books.Add(book3);
+
+                _context.SaveChanges();
+            }
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> Get()
+        {
+            return _context.Books.ToList();
+        }
+
+        public User GetUser()
+        {
+            var token = GetToken();
+            var user = _context.Users.Where(u => u.Id == token).FirstOrDefault();
+            return user;
+        }
+
+        public string GetToken()
+        {
+            return Request.Headers["Authorization"].FirstOrDefault();
+        }
+
+    }
+}
---
+++ MyReadsWebApi/Models/Models.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyReadsWebApi.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+            this.Database.EnsureDeleted();
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+    }
+
+    public class User
+    {
+        public string Id { get; set; }
+        public List<Book> Books { get; set; }
+    }
+
+    public class Author
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Book
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public List<Author> Authors { get; set; }
+        public string Shelf { get; set; }
+    }
+
+    public class ImageLink
+    {
+        public string Thumbnail { get; set; }
+        public string SmallThumbnail { get; set; }
+    }
+
+
+}
